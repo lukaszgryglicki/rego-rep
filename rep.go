7b3cbd30811c864edda52f6f753bdd12e536dd0e
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-02-20 08:02:04 +0000

    Final report
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- rep.go
+++ rep.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -13,6 +16,7 @@ const (
 )
 
 func rep() error {
+	warn := os.Getenv("WARN") != ""
 	shas := os.Getenv("SHAS")
 	if shas == "" {
 		shas = gSHAs
@@ -31,6 +35,7 @@ func rep() error {
 	shasm2 := make(map[string]map[string]struct{})
 	for shass.Scan() {
 		line := shass.Text()
+		// 5d8bceaffa4a3368d14cc2d321cdd2fad5ffb1c94ee2c05119c3053758bed4ebeffa8eb260699c437c9db34ef3e024150aa2c6ec953882b45bf8bd4c17292c24  ./envoyproxy/archive/docs/envoy/v1.21.3/_downloads/820c9994319e32174253485940818917/policy.rego
 		ary := strings.Fields(line)
 		sha := ary[0]
 		fn := strings.Join(ary[1:], " ")
@@ -55,9 +60,66 @@ func rep() error {
 		}
 	}
 	for k, v := range shasm2 {
-		if len(v) > 1 {
+		if warn && len(v) > 1 {
 			fmt.Printf("SHA '%s' matches multiple files: %+v\n", k, v)
 		}
+	}
+	timesf, err := os.Open(times)
+	if err != nil {
+		return err
+	}
+	defer timesf.Close()
+	timess := bufio.NewScanner(timesf)
+	years := make(map[string]map[string]struct{})
+	for timess.Scan() {
+		line := timess.Text()
+		// -rw-r--r-- 1 root root 232 2023 ./envoyproxy/archive/docs/envoy/v1.24.2/_downloads/820c9994319e32174253485940818917/policy.rego
+		ary := strings.Fields(line)
+		year := ary[5]
+		yeari, err := strconv.Atoi(year)
+		if err != nil {
+			fmt.Printf("incorrect year value: '%s'\n", year)
+			continue
+		}
+		if yeari < 2000 || yeari > time.Now().Year() {
+			fmt.Printf("incorrect year value: '%s' -> %d\n", year, yeari)
+			continue
+		}
+		fn := strings.Join(ary[6:], " ")
+		mp, ok := shasm1[fn]
+		if !ok {
+			fmt.Printf("there is no SHA value for '%s' file (year %s)\n", fn, year)
+			continue
+		}
+		var sha string
+		for v := range mp {
+			sha = v
+			break
+		}
+		_, ok = years[year]
+		if !ok {
+			years[year] = make(map[string]struct{})
+		}
+		years[year][sha] = struct{}{}
+	}
+	err = timess.Err()
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("%+v\n", years)
+	yearss := []string{}
+	for year := range years {
+		yearss = append(yearss, year)
+	}
+	sort.Strings(yearss)
+	fmt.Printf("year,\"rego files\"\n")
+	for _, year := range yearss {
+		shas, ok := years[year]
+		if !ok {
+			fmt.Printf("year %s not present in map +%v\n", year, years)
+			continue
+		}
+		fmt.Printf("%s,%d\n", year, len(shas))
 	}
 	return nil
 }
